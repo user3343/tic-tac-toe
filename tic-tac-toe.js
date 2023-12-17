a327c88cdd1db853c3423620449ea373f27519ca
Baljot Singh <baljotsingh3343@gmail.com>
2023-12-17 19:25:37 +0530

    game done

---
+++ tic-tac-toe.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset");
+let newGameBtn = document.querySelector(".new");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector(".msg");
+let turnO = true;
+
+const winPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO === true) {
+      box.innerText = "x";
+      box.style.color = "blue";
+      turnO = false;
+    } else {
+      box.innerText = "o";
+      box.style.color = "green";
+      turnO = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const disableBoxes = () => {
+  for (const box of boxes) {
+    box.disabled = true;
+  }
+};
+const enableBoxes = () => {
+  for (const box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `winner is ${winner}`;
+  //   msg.innerText = `fuck you 🖕`;
+
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPattern) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("winner", pos1Val);
+        showWinner(pos1Val);
+      }
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
